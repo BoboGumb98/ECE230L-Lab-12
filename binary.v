67eac6a8178261481c50cc847b8317d897119c9a
Brolen Gumb <Brolengumb@u.boisestate.edu>
2026-04-14 11:17:59 -0600

    Refactor binary.v state logic and DFF connections

--- binary.v
+++ binary.v
@@ -5,38 +5,42 @@ module binary (
     input clk,
     input reset,
     output z,
-    output wire [2:0] StateOut
+    output [2:0] State
 );
 
-    wire [2:0] State;
+  //  [2:0] State;
     wire [2:0] Next;
+    
     
     dff zero(
         .D(Next[0]),
         .clk(clk),
         .Q(State[0]),
-        .reset(reset)
+        .reset(reset),
+        .Default(1'b0)
     );
     
     dff one(
         .D(Next[1]),
         .clk(clk),
         .Q(State[1]),
-        .reset(reset)
+        .reset(reset),
+        .Default(1'b0)
     );
     
     dff two(
         .D(Next[2]),
         .clk(clk),
         .Q(State[2]),
-        .reset(reset)
+        .reset(reset),
+        .Default(1'b0)
     );
     
     assign z = State[2] | (State[1] & ~State[0]);
-    assign Next[0] = (~State[1] & ~State[0] & ~w) | (State[1] & ~State[0] & w)
-                   | (~State[2] & ~State[1] & w) | ( State[1] & State[0] & ~w);
-    assign Next[1] = (State[1] & ~State[0]) | (~State[2] & ~State[0] & ~w)
-                   | (~State[2] & ~State[1] & State[0]);
+    assign Next[0] = (State[1] & State[0] & ~w) | (~State[2] & ~State[0] & w)
+                   | (~State[2] & ~State[1] & w) | (~State[1] & ~State[0] & ~w);
+    assign Next[1] = (State[1] & ~State[0]) | (~State[1] & State[0])
+                   | (~State[2] & ~State[1] & w);
     assign Next[2] = (State[2] & w) | (State[1] & State[0] & w);
 
 //    A = 3'b000;
